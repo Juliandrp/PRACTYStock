3e53218364872ea8e69302478449f44169e99dd8
Juliandrp <38564304+Juliandrp@users.noreply.github.com>
2018-04-24 11:49:16 -0500

    login finalizado, ventas solo falta acomodar la imagen del parallax al tamaño de la pantalla

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent }   from './components/login/login.component';
+import { VentasComponent } from './components/ventas/ventas.component';
+
 
 
 const routes: Routes = [
   //{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '', component: LoginComponent },
+  { path: 'admin/ventas', component: VentasComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MzSelectModule } from 'ng2-materialize';
 import { MzButtonModule, MzInputModule } from 'ng2-materialize';
-import { MzSidenavModule } from 'ng2-materialize'
+import { MzCardModule } from 'ng2-materialize'
+import { MzSidenavModule } from 'ng2-materialize';
+import { MzParallaxModule } from 'ng2-materialize'
 import { AppRoutingModule }     from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { VentasComponent } from './components/ventas/ventas.component';
+import { EquiposComponent } from './components/equipos/equipos.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    VentasComponent,
+    EquiposComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MzSidenavModule,
     MzButtonModule,
+    MzParallaxModule,
    MzInputModule,
+   MzSelectModule,
+   MzCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
