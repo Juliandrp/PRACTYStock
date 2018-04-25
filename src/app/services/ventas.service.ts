dad7facc876f4b2224b5c630520bcf3c39fd98de
black <joseb.twelve@gmail.com>
2018-04-25 10:45:11 -0500

    servicios creados e inyectados

--- src/app/services/ventas.service.ts
+++ src/app/services/ventas.service.ts
@@ -2,29 +2,29 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable()
-export class VentaService {
-  url:string = 'practy-project.herokuapp.com';
+export class VentasService {
+  url:string = 'https://practy-project.herokuapp.com';
 
   constructor(
     private http:HttpClient
   ) { }
 
-  sold(token,data) {
+  tokenizer():Promise<any> {
     return new Promise((resolve,reject) => {
-      this.http.post(`${this.url}/ventas`,token+data)
-        .subscribe( resp => {
-          resolve(resp);
+      this.http.get(`${this.url}/login`)
+        .subscribe(token => {
+          resolve(token)
         },err => {
           reject(err);
         })
-    });
+    })
   }
 
-  getVentas(id) {
+  sold(token,data):Promise<any> {
     return new Promise((resolve,reject) => {
-      this.http.get(`${this.url}/venta/${id}`)
-        .subscribe( data => {
-          resolve(data);
+      this.http.post(`${this.url}/ventas`,token+data)
+        .subscribe(resp => {
+          resolve(resp);
         },err => {
           reject(err);
         })
