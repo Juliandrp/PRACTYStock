4396eb846f4aa8235017d8f795ff34780d8b98d4
black <joseb.twelve@gmail.com>
2018-04-25 16:52:03 -0500

    arreglado bug de la vista ventas

--- src/app/components/ventas/ventas.component.ts
+++ src/app/components/ventas/ventas.component.ts
@@ -12,27 +12,19 @@ import { LoginComponent } from '../login/login.component';
 })
 export class VentasComponent implements OnInit {
   venta:any;
-  _token:any;
-
+  
   constructor(
     private service:VentasService,
     private _toast:MzToastService,
-    private log:LoginComponent
   ) { 
 
   }
 
   ngOnInit() {
-    this._token = this.log.getToken();
   }
 
   postVentas():void {
-    this.service.tokenizer().then(token => {
-      this._token = token;
-    }).catch( err => {
-      this._toast.show(<string>err,4000,'black');
-    });
-    this.service.sold(this._token,this.venta).
+    this.service.sold(this.venta).
       then(data => {
         this._toast.show(<string>data,400,'black');
       }).catch(err => {
